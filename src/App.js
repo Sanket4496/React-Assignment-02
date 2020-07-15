10984d92ab3297d8590d013c972470be671f89c9
sanket <sanket.panda@viacom18.com>
2020-07-15 23:48:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import './Validation/Validation'
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    userInput : ''
+  }
+
+  inputChangeListener = (event) => {
+    this.setState({userInput : event.target.value})
+  };
+
+  deleteCharacter = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index,1);
+    const updatedText = text.join('');
+    this.setState({userInput : updatedText});
+  }
+
+
+  render(){
+    const charList = this.state.userInput.split('').map((ch,index) => {
+      return <Char
+      character = {ch}
+      key = {index}
+      clicked = {() => this.deleteCharacter(index)} 
+      />
+    }
+    )
+
+    return (
+      <div className="App">
+      <input onChange = {(event) => this.inputChangeListener(event)} value = {this.state.userInput}/>
+      <p> The size of the input : {this.state.userInput.length}</p>
+      <Validation inputLength = {this.state.userInput.length}/>
+      {charList}
+      </div>
+    );
+  }  
 }
 
 export default App;
